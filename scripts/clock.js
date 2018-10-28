44858f0aba1a698cd7f15abbc8383e4579e8a2d2
Dante Naid Lua Gómez <naidlua@mx1.ibm.com>
2018-10-28 17:48:55 -0600

    Clock replacement and ordering

---
+++ scripts/clock.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Time right now is: </title>
+  <script>
+    var time = new Date();
+    console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
+  </script>
+</head>
+
+<body>
+  <script>∫
+    Java Script Code Also Here
+  </script>
+</body>
+
+</html>
