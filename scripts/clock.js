e27971960d5b63b1e64fe8dbb17a5c1df28b7ce4
Dante Naid Lua Gómez <naidlua@mx1.ibm.com>
2018-10-28 18:07:05 -0600

    JS code separartion

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en-US">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <title>Time right now is: </title>
-  <script>
-    var time = new Date();
-    console.log(time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
-  </script>
-</head>
-
-<body>
-  <script>∫
-    Java Script Code Also Here
-  </script>
-</body>
-
-</html>
+let d = new Date();
+document.body.innerHTML = "<h1> Time right now is: " + d.getHours() +
+ ":" + d.getMinutes + ":" + d.getSeconds()</h1>"
